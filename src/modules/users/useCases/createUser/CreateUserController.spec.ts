bd64a8b69b06d0071ea3a179fb7cf6d45f5d6313
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-05-07 23:43:24 -0300

    fixed the route on CreateUserController.spec.ts

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -5,12 +5,14 @@ import { app } from "../../../../app";
 describe("Create Category Controller", () => {
     it("Should be able to create a new user", async () => {
         const response = await request(app)
-            .post("/users")
+            .post("/api/v1/users")
             .send({
                 name: "Supertest Name",
                 email: "supertest@email.com",
                 password: "supertestpassword",
             });
+
+        console.log(response.error)
 
         expect(response.status).toBe(201);
     })
