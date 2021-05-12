e204e69d19a0c1f8dc7ff52300a403085d9c7620
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-05-12 11:39:35 -0300

    AuthenticateUser and ShowUserProfile tests working

--- src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -6,8 +6,6 @@ import createDbConnection from '../../../../database';
 
 let connection: Connection;
 
-
-
 describe("Authenticate User Controller", () => {
 
     beforeAll(async () => {
@@ -38,15 +36,27 @@ describe("Authenticate User Controller", () => {
         expect(response.body).toHaveProperty("token");
     });
 
-    // it("Should not be able to create a new user with same e-mail", async () => {
-    //     const response = await request(app)
-    //         .post("/api/v1/users")
-    //         .send({
-    //             name: "Supertest Name2",
-    //             email: "supertest@email.com",
-    //             password: "supertestpassword2",
-    //         });
+    it("Should not be able to authenticate an user with wrong e-mail", async () => {
+        const response = await request(app)
+            .post("/api/v1/sessions")
+            .send({
+                email: "wrong@email.com",
+                password: "supertestpassword",
+            });
 
-    //     expect(response.status).toBe(400);
-    // });
+        expect(response.status).toBe(401);
+        expect(response.text).toEqual("{\"message\":\"Incorrect email or password\"}");
+    });
+
+    it("Should not be able to authenticate an user with wrong password", async () => {
+        const response = await request(app)
+            .post("/api/v1/sessions")
+            .send({
+                email: "supertest@email.com",
+                password: "wrongpassword",
+            });
+
+        expect(response.status).toBe(401);
+        expect(response.text).toEqual("{\"message\":\"Incorrect email or password\"}");
+    });
 });
---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -0,0 +1,73 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+import { app } from "../../../../app";
+import { hash } from "bcryptjs"
+
+import createDbConnection from '../../../../database';
+import { UsersRepository } from "../../repositories/UsersRepository";
+
+let connection: Connection;
+let usersRepository: UsersRepository;
+
+describe("Authenticate User Controller", () => {
+
+    beforeAll(async () => {
+        connection = await createDbConnection();
+        await connection.runMigrations();
+        usersRepository = new UsersRepository();
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    })
+
+    it("Should be able to show user profile", async () => {
+        const email = "supertest@email.com";
+        const password = "supertestpassword";
+
+        const user = await usersRepository.create({
+            name: "Supertest Name",
+            email: "supertest@email.com",
+            password: await hash(password, 8),
+        });
+
+        const id = user.id;
+
+        const authenticate = await request(app)
+            .post("/api/v1/sessions")
+            .send({
+                email,
+                password,
+            });
+
+        const { token } = authenticate.body;
+
+        const response = await request(app)
+            .get("/api/v1/profile")
+            .send(id)
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.body).toHaveProperty("name");
+        expect(response.body).toHaveProperty("created_at");
+        expect(response.body).toHaveProperty("updated_at");
+    });
+
+    it("Should not be able to show user profile without authenticating", async () => {
+        const user = await usersRepository.create({
+            name: "Supertest Name2",
+            email: "supertest@email2.com",
+            password: await hash("supertestpassword2", 8),
+        });
+
+        const id = user.id;
+
+        const response = await request(app)
+            .get("/api/v1/profile")
+            .send(id);
+
+        expect(response.status).toBe(401);
+    });
+});
